075da9997938d1ae87cf79b2e25b6894cb9af0bb
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-06-28 09:35:53 +0530

    add the services

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./styles/global.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+// 1. ADD THIS LINE: Import the new component you just created
+import ServicesOverview from "./components/ServicesOverview"; 
 
 export default function App() {
   return (
@@ -8,6 +10,9 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        {/* 2. ADD THIS LINE: Place the component right below the Hero */}
+        <ServicesOverview />
+        
         {/* Future sections: Fleet showcase, Why Oneness, Testimonials, etc. */}
       </main>
     </>
