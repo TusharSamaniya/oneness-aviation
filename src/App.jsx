e98e12fd9da83bb373d4c7cfd2aa6c09b7106cc6
Tusha Samaniya <tusharsamaniya02@gmail.com>
2026-06-28 11:45:01 +0530

    remove everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import "./styles/global.css";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-// 1. ADD THIS LINE: Import the new component you just created
-import ServicesOverview from "./components/ServicesOverview"; 
-import HowItWorks from "./components/HowItWorks";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <main>
-        <Hero />
-        {/* 2. ADD THIS LINE: Place the component right below the Hero */}
-        <ServicesOverview />
-        <HowItWorks /> {/* Add this line */}
+        
       </main>
     </>
   );
